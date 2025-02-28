79e9ea8ad9f4f562dee01ad78a19966d8333681e
Salman-Ahamed <me.shahriyarhosen@gmail.com>
2025-03-01 02:48:02 +0600

    Enhance FavoritesPage and ProfilePage: implement conditional rendering for empty states; add SVG icons for better user experience; update ListItem component usage

--- src/app/(wrapper)/(personal)/favorites/page.tsx
+++ src/app/(wrapper)/(personal)/favorites/page.tsx
@@ -1,5 +1,24 @@
+import { ListItem } from "@/components/shared";
+import { IRecipe } from "@/interface";
+import Image from "next/image";
+
 const FavoritesPage = () => {
-  return <div className="">FavoritesPage</div>;
+  const data: IRecipe[] = []; // recipes.slice(0, 6);
+
+  return (
+    <div className="pb-40">
+      {data.length > 0 ? (
+        <ListItem data={data} />
+      ) : (
+        <div className=" flex flex-col w-full h-[60svh] justify-center items-center gap-[18px]">
+          <Image alt="no data" src="/fav.svg" width={85} height={85} />
+          <p className="text-center text-2xl text-[#656565] font-semibold max-w-[250px]">
+            You don&apos;t have any favorite recipes yet.
+          </p>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default FavoritesPage;
--- src/app/(wrapper)/(personal)/profile/page.tsx
+++ src/app/(wrapper)/(personal)/profile/page.tsx
@@ -1,10 +1,23 @@
 import { ListItem } from "@/components/shared";
+import { IRecipe } from "@/interface";
 import { recipes } from "@/lib/data";
+import Image from "next/image";
 
 const ProfilePage = () => {
+  const data: IRecipe[] = recipes.slice(0, 6);
+
   return (
     <div className="pb-40">
-      <ListItem isProfile data={recipes.slice(0, 6)} />
+      {data.length > 0 ? (
+        <ListItem isProfile data={data} />
+      ) : (
+        <div className=" flex flex-col w-full h-[60svh] justify-center items-center gap-[18px]">
+          <Image alt="no data" src="/add.svg" width={85} height={85} />
+          <p className="text-center text-2xl text-[#656565] font-semibold max-w-[250px]">
+            Post your first recipe here!
+          </p>
+        </div>
+      )}
     </div>
   );
 };
